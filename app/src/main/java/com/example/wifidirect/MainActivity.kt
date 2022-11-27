072d3dd745e2aa50ad90819cec0cf1087c5b3aaf
Алексей Шемет <shemet162534@gmail.com>
2022-11-28 01:57:15 +0300

    Приложение, что грузит фоточки марса Фрагмент детаил еще не отредактирован

--- app/src/main/java/com/example/wifidirect/MainActivity.kt
+++ app/src/main/java/com/example/wifidirect/MainActivity.kt
@@ -1,11 +1,74 @@
 package com.example.wifidirect
+/*
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.IntentFilter
+import android.net.wifi.p2p.WifiP2pManager
+import android.os.Build.VERSION_CODES.R
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.wifidirect.network.wifidirect.WiFiDirectBroadcastReceiver
+
+class MainActivity : AppCompatActivity() {
+
+    val manager: WifiP2pManager? by lazy(LazyThreadSafetyMode.NONE) {
+        getSystemService(Context.WIFI_P2P_SERVICE) as WifiP2pManager?
+    }
+
+    var channel: WifiP2pManager.Channel? = null
+    var receiver: BroadcastReceiver? = null
+
+    val intentFilter = IntentFilter().apply {
+        addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION)
+        addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION)
+        addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION)
+        addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        channel = manager?.initialize(this, mainLooper, null)
+        channel?.also { channel ->
+            receiver = manager?.let { WiFiDirectBroadcastReceiver(it, channel, this) }
+        }
+
+        val binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        receiver?.also { receiver ->
+            registerReceiver(receiver, intentFilter)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        receiver?.also { receiver ->
+            unregisterReceiver(receiver)
+        }
+    }
+
+}*/
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    /**
+     * Our MainActivity is only responsible for setting the content view that contains the
+     * Navigation Host.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 }
+
+
+
+
